848228f585dc0083370824058a0e4292a1112c06
Trogg2000 <eldestharvie@gmail.com>
2025-02-24 11:10:15 -0800

    add updatable and drawable groups

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -19,10 +23,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)         
+        updatable.update(dt)         
 
         screen.fill((0,0,0))
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -15,12 +16,14 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-    def move(self, dt):
 
+    def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
